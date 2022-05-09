ab9cee5f2b829a79e67143a3dffa76b8129c59a9
CarlosDaniel396 <carlosdaniel44u@gmail.com>
2022-05-09 15:01:52 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
+import Navbar from "components/Navbar";
+import Form from "pages/Form";
+import Listing from "pages/Listing";
 
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
